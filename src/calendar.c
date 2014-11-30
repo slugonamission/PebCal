075ca83d5ca7b2b9ffa75a4947e282bb85ba5111
Jamie <slugonamission@users.noreply.github.com>
2014-11-30 15:06:46 +0000

    Dynamically get month start, length and weeks.

--- src/calendar.c
+++ src/calendar.c
@@ -13,6 +13,13 @@
 #define CALENDARPANE_Y_MARGIN 5 // Space at the bottom
 #define CALENDARPANE_NUMBER_X_PAD 2
     
+// Which is the start day of the week?
+// This offset is added to dow below to shift
+// it slightly.
+#define WEEK_START_SUN 0
+#define WEEK_START_MON 1
+#define WEEK_START_OFFSET WEEK_START_MON
+    
 typedef struct
 {
     Layer* graphicsLayer;
@@ -23,6 +30,25 @@ void calendar_graphics_draw(Layer* layer, GContext* context);
     
 // Draw a single calendar
 void calendar_single_draw(Layer* layer, GContext* context, int16_t offset);
+
+// Monthno should be 1-12
+int days_in_month(int monthNo, int yearNo);
+
+int days_in_month(int monthNo, int yearNo)
+{
+    // Borrowed from http://www.codecodex.com/wiki/Calculate_the_number_of_days_in_a_month
+    if(monthNo < 1 || monthNo > 12)
+        return -1;
+    
+    if(monthNo == 4 || monthNo == 6 || monthNo == 9 || monthNo == 11)
+        return 30;
+    if(monthNo == 2)
+    {
+        bool isLeapYear = (yearNo % 4 == 0 && yearNo % 100 != 0) || (yearNo % 400 == 0);
+        return isLeapYear ? 29 : 28;
+    }
+    return 31;
+}
 
 void calendar_load(Window* wnd)
 {
@@ -87,6 +113,34 @@ void calendar_single_draw(Layer* layer, GContext* context, int16_t offset)
     char month[20];
     strftime(month, 20, "%B %Y", localTime);
     
+    // Get the first day of the month (Mon-Sun)
+    // Get the current day and day of week
+    int curDay = localTime->tm_mday;
+    int dow = localTime->tm_wday;
+    
+    // What's the offset into dow?
+    // Ignoring weeks, this will give us the number of days
+    // since the first of the month
+    int curDowOffset = (curDay - 1 + WEEK_START_OFFSET) % 7;
+    
+    // We then subtract that from the current day of the week
+    // which will yield the dow of the first day of the month.
+    dow -= curDowOffset;
+    if(dow < 0)
+        dow += 7;
+    
+    // Get the number of days to draw
+    int days_month = days_in_month(localTime->tm_mon + 1, localTime->tm_year + 1900);
+    
+    // How many weeks do we need to show?
+    // How many days are there, including the leadin?
+    int totalSlots = (days_month + dow);
+    int lines = totalSlots / 7;
+    
+    // I'm not resorting to the FPU. Check if it had a fractional part
+    if(lines * 7 != totalSlots)
+        lines++;
+    
     // Create the bounding box for the period
     GRect contextBound = layer_get_frame(layer);
     GRect textBounding = contextBound;
@@ -130,9 +184,9 @@ void calendar_single_draw(Layer* layer, GContext* context, int16_t offset)
                           (GPoint){CALENDARPANE_X_MARGIN + vertSubDiv*i, CALENDARPANE_TOP + offset + calHeight});
     }
     
-    // And now the horizontal subdivisions. There needs to be 5
-    int horizSubDiv = calHeight / 5;
-    for(int i = 1; i < 5; i++)
+    // And now the horizontal subdivisions.
+    int horizSubDiv = calHeight / lines;
+    for(int i = 1; i < lines; i++)
     {
         graphics_draw_line(context,
                            (GPoint){CALENDARPANE_X_MARGIN, CALENDARPANE_TOP + offset + horizSubDiv*i},
@@ -140,10 +194,10 @@ void calendar_single_draw(Layer* layer, GContext* context, int16_t offset)
     }
     
     // Now draw the days
-    for(int day = 1; day <= 31; day++)
+    for(int day = 1; day <= days_month; day++)
     {
-        int x = (day-1) % 7;
-        int y = (day-1) / 7;
+        int x = (day-1+dow) % 7;
+        int y = (day-1+dow) / 7;
         char dayStr[3];
         snprintf(dayStr, 3, "%d", day);
         
